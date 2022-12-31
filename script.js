9b8203370009e8ba040407d83e966aeee9ec95e2
angenue <angelica1nuesi@gmail.com>
2022-12-31 14:48:02 -0500

    gave div and id and class name

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 //adding more divs inside container
-const container = document.querySelector('#container');
+const container = document.querySelector('.container');
 const squareGrid = 256; //size of 16 by 16 grid
-for (let i=0; i <= squareGrid; i++) { 
-    let div = document.createElement('div');
+for (let i=1; i <= squareGrid; i++) { 
+    const div = document.createElement('div');
+    div.id = 'square' + i;
+    div.className = 'square';
     container.appendChild(div);
 }
+document.appendChild(container);
