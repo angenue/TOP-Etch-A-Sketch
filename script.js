cb3837538f347ec3693f8ba56449fc425a559a19
angenue <angelica1nuesi@gmail.com>
2023-01-01 16:37:51 -0500

    deletes previous grids and makes new one

--- script.js
+++ script.js
@@ -1,29 +1,45 @@
-//adding more divs inside container
 const container = document.querySelector('.container');
+const slider = document.getElementById("myRange");
+const output = document.getElementById("demo");
 
-const squareGrid = 256; //size of 16 by 16 grid
-for (let i = 1; i <= squareGrid; i++) {  //creates 256 square divs
+const defaultGrid = 32 * 32;
+for (let i = 1; i <= defaultGrid; i++) {  //creates 256 square divs
     const newDiv = document.createElement('div');
     newDiv.id = 'square' + i;
     newDiv.className = 'square';
     container.appendChild(newDiv);
 }
+
+output.innerHTML = slider.value; //displaying value
+slider.oninput = function () { //changes number when slider is moved
+    output.innerHTML = this.value;
+
+    const value = (this.value - this.min) / (this.max - this.min) * 100;
+    this.style.background = 'linear-gradient(to right, #FFDE55 0%, #74DAE2 ' + value + '%, #fff ' + value + '%, #fff 100%)';
+
+    //adding more divs inside container
+    let row = this.value;
+    let column = this.value;
+    const squareGrid = row * column; //size of 16 by 16 grid
+
+while (container.firstChild) {
+    container.removeChild(container.firstChild);
+}
+
+for (let i = 1; i <= squareGrid; i++) {  //creates 256 square divs
+    const newDiv = document.createElement('div');
+    newDiv.id = 'square' + i;
+    newDiv.className = 'square';
+    container.appendChild(newDiv);
+
+    container.style.gridTemplateColumns = 'repeat(' + this.value +', 1fr)';
+}
+
+};
+
+
 document.appendChild(container);
 
-
-
-var slider = document.getElementById("myRange");
-var output = document.getElementById("demo");
-output.innerHTML = slider.value;
-
-slider.oninput = function () {
-
-    const value = (this.value - this.min) / (thix.max - this.min) * 100;
-
-    this.style.background = 'linear-gradient(to right, #6b8dff 0%, #ff2a5f ' + value + ', #fff ' + value + ', #fff 100%)';
-    output.innerHTML = slider.value;
-    output.innerHTML = this.value;
-};
 
 
 
