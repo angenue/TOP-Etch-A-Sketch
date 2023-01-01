d32bba996daea8bd98a5c9bcbed8a1266a98af88
angenue <angelica1nuesi@gmail.com>
2023-01-01 18:41:22 -0500

    moved for loop in oninput function

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const slider = document.getElementById("myRange");
 const output = document.getElementById("demo");
 
 const defaultGrid = 32 * 32;
-for (let i = 1; i <= defaultGrid; i++) {  //creates 256 square divs
+for (let i = 1; i <= defaultGrid; i++) {  //creates 32 by 32 square divs
     const newDiv = document.createElement('div');
     newDiv.id = 'square' + i;
     newDiv.className = 'square';
@@ -14,19 +14,18 @@ output.innerHTML = slider.value; //displaying value
 slider.oninput = function () { //changes number when slider is moved
     output.innerHTML = this.value;
 
-    const value = (this.value - this.min) / (this.max - this.min) * 100;
+    const value = (this.value - this.min) / (this.max - this.min) * 100; //changges slider color
     this.style.background = 'linear-gradient(to right, #FFDE55 0%, #74DAE2 ' + value + '%, #fff ' + value + '%, #fff 100%)';
 
-    //adding more divs inside container
-    let row = this.value;
-    let column = this.value;
-    const squareGrid = row * column; //size of 16 by 16 grid
-
-while (container.firstChild) {
+//deletes all child elements of container
+while (container.firstChild) { 
     container.removeChild(container.firstChild);
 }
 
-for (let i = 1; i <= squareGrid; i++) {  //creates 256 square divs
+//adding more divs inside container
+const squareGrid = this.value * this.value; //taking slider value to get grid size
+
+for (let i = 1; i <= squareGrid; i++) {  
     const newDiv = document.createElement('div');
     newDiv.id = 'square' + i;
     newDiv.className = 'square';
