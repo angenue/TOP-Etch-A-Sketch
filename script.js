d45ce26296b67025158f606c0d3d0c77381d7456
angenue <angelica1nuesi@gmail.com>
2023-01-05 15:50:23 -0500

    created seperate function for grid size

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-let currentColor = 'black';
-
+let currentColor = '#333333';
+let mouseDown = false; //for mouse event listeners
+const defaultGrid = 32; //grid default value
 
 const container = document.querySelector(".container");
 const slider = document.getElementById("myRange");
 const output = document.getElementById("demo");
-const solidColor = document.getElementById("solid")//.addEventListener("click", changeToSolid);
-const rainbowColor = document.getElementById("rainbow")//.addEventListener("click", changeToRainbow);
-const eraseButton = document.getElementById("erase")//.addEventListener("click", eraseGrid);
-const resetButton = document.getElementById("reset")//.addEventListener("click", resetGrid);
+const solidColor = document.getElementById("solid")
+const rainbowColor = document.getElementById("rainbow")
+const eraseButton = document.getElementById("erase")
+const resetButton = document.getElementById("reset").addEventListener("click", resetGrid);
 
 
 solidColor.oninput = (e) => changeColor(e.target.value);
@@ -15,22 +16,9 @@ solidColor.onclick = (e) => changeColor(e.target.value);
 rainbowColor.onclick = () => changeColor('rainbow');
 eraseButton.onclick = () => changeColor('white');
 
-let mouseDown = false; //for mouse event listeners
-//document.body.onmousedown = () => (mouseDown = true)
-//document.body.onmouseup = () => (mouseDown = false)
-
-
+createGrid(defaultGrid); //creating default grid
 output.innerHTML = slider.value; //displaying slider value
-const defaultGrid = 32 * 32; //grid default value
 
-
-//creating default grid
-for (let i = 1; i <= defaultGrid; i++) { //creates 32 by 32 square divs
-  const newDiv = document.createElement("div");
-  newDiv.id = "square" + i;
-  newDiv.className = "square";
-  container.appendChild(newDiv);
-};
 
 //changes number when slider is moved and creates new grid
 slider.oninput = function () {
@@ -42,15 +30,25 @@ slider.oninput = function () {
   while (container.firstChild) {
     container.removeChild(container.firstChild);
   }
+
+  createGrid(this.value)
+  
+};
+
+
+function createGrid(size) {
   //adding more divs inside container
-  const squareGrid = this.value * this.value; //taking slider value to get grid size
-  for (let i = 1; i <= squareGrid; i++) {
+  //const squareGrid = `${value} x ${value}`; //taking slider value to get grid size
+  for (let i = 0; i < size * size; i++) {
     const newDiv = document.createElement("div");
     newDiv.className = "square";
     container.appendChild(newDiv);
   }
-  container.style.gridTemplateColumns = "repeat(" + this.value + ", 1fr)"; //changes row amount
-};
+  container.style.gridTemplateColumns = `repeat(${size}, 1fr)`; //changes row amount
+}
+
+
+
 
 container.addEventListener("mousedown", (e) => {
   if (currentColor === "rainbow") {
@@ -74,87 +72,10 @@ container.addEventListener("mouseup", () => {
   mouseDown = false;
 });
 
-  
-
 
 function changeColor(colorChoice) {
   currentColor = colorChoice;
 }
-
-//changes color of divs to black on click
-/*function changeToDefault() {
-  container.addEventListener("mousedown", (event) => {
-    //event delegation. Using container instead of each grid
-    event.target.style.backgroundColor = "#333333";
-    mouseDown = true;
-    container.addEventListener("mouseover", (event) => {
-      if (mouseDown) {
-        event.target.style.backgroundColor = "#333333";
-      }
-    });
-  });
-  container.addEventListener("mouseup", () => {
-    mouseDown = false;
-  });
-}
-
-//changes color based on color input
-solidColor.oninput = function () {
-  solidColor.addEventListener('click', () => {
-    container.addEventListener("mousedown", (event) => {
-      //event delegation. Using container instead of each grid
-      event.target.style.backgroundColor = this.value;
-      mouseDown = true;
-      container.addEventListener("mouseover", (event) => {
-        if (mouseDown) {
-          event.target.style.backgroundColor = this.value;
-        }
-      });
-    });
-    container.addEventListener("mouseup", () => {
-      mouseDown = false;
-    });
-  })
-  }
-
-
-//changes color of divs to rainbow on click
-function changeToRainbow() {
-  container.addEventListener("mousedown", (event) => {
-    //event delegation. Using container instead of each grid
-    event.target.style.backgroundColor = `hsl(${
-      Math.random() * 360
-    }, 100%, 50%)`;
-    mouseDown = true;
-    container.addEventListener("mouseover", (event) => {
-      if (mouseDown) {
-        event.target.style.backgroundColor = `hsl(${
-          Math.random() * 360
-        }, 100%, 50%)`;
-      }
-    });
-  });
-  container.addEventListener("mouseup", () => {
-    mouseDown = false;
-  });
-}
-
-//changes color of divs to white on click
-function eraseGrid() {
-  container.addEventListener("mousedown", (event) => {
-    //event delegation. Using container instead of each grid
-    event.target.style.backgroundColor = "white";
-    mouseDown = true;
-    container.addEventListener("mouseover", (event) => {
-      if (mouseDown) {
-        event.target.style.backgroundColor = "white";
-      }
-    });
-  });
-  container.addEventListener("mouseup", () => {
-    mouseDown = false;
-  });
-} */
 
 //erases grid by changing all divs to white
 function resetGrid() {
@@ -162,6 +83,5 @@ function resetGrid() {
     (child) => (child.style.backgroundColor = "white")
   );
 } 
-
 
 document.appendChild(container);
